8eb44b4b516db760f2667be5abb5d2070b45b891
ing.conti <ingconti@ingconti.com>
2025-05-10 15:21:09 +0200

    swing 07 small model

---
+++ src/main/java/it/polimi/SmallModel.java
@@ -0,0 +1,15 @@
+package it.polimi;
+
+
+public class SmallModel {
+
+    private String timeStr = "??";
+
+    public String getTimeStr() {
+        return timeStr;
+    }
+
+    public void setTimeStr(String timeStr) {
+        this.timeStr = timeStr;
+    }
+}
